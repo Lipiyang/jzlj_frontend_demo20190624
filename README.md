106875833f2b641d023bfb5fda4c5f9dc32281f6
Lipiyang <2846870147@qq.com>
2019-06-24 20:49:37 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # jzlj_frontend_demo20190624
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
+因为还有一个登录界面要使用APP.vue页面，所以公共的标题栏放到每个人写的代码下面了
+
---
+++ src/components/WasteResuseInfo/Home.vue
@@ -0,0 +1,186 @@
+<template>
+<div>    
+ <div class="header left">
+    <div class="left nav itemDiv">
+        <ul>
+            <li><i class="nav_1"></i>数据概览 </li>
+            <li><i class="nav_2"></i>车辆监控</li>
+            <li><i class="nav_3"></i>地图界面 </li>
+            </ul>
+    </div>
+    <div class="header_center left itemDiv">
+        <h2><strong>建筑垃圾全过程实时监测与智能管控云平台</strong></h2>
+        <p class="color_font"><small>Comprehensive service platform for smart tourism</small></p>
+    </div>
+    <div class="right nav text_right itemDiv">
+        <ul> <li><i class="nav_7"></i>查询统计 </li>
+            <li><i class="nav_8"></i>信息录入 </li>
+            <li><i class="nav_4"></i>表格界面 </li>
+        </ul>
+    </div>
+    </div>
+    <router-view/>
+</div>
+
+</template>
+<script>
+
+export default {
+ 
+}
+</script>
+
+<style>
+.header{
+    width: 100%;
+    height: 50px;
+    background-color: #030829;
+    display: table;
+    table-layout: fixed;
+    /* background-size: 100% 100%; */
+    
+}
+.itemDiv{
+    display:table-cell;
+  
+}
+/* ../src/assets/img/logoBg.png */
+.header_center{
+    width: 30%;
+    margin: 0px auto;
+    color: #FFFFff;
+    text-align: center;
+    height: 50px;
+    background-image: url("../WasteResuseInfo/img/logoBg.png");
+    background-size: 100% 100%;
+    font-family: "微软雅黑";
+    /* padding:5px; */
+}
+
+.header_center h2{
+   margin-top: 10px ;
+    margin-bottom: 10px;
+    font-size: 18px ;
+}
+
+.color_font{
+    color: #e8f7fe; 
+    font-size: 12px ;
+}
+
+.header_logo{
+    margin-left: 1%;
+    margin-top: 12px;
+
+}
+
+.header_logo img{
+    height: 56px;
+}
+
+.nav{
+
+    width: 35%;
+   
+}
+.nav>ul{
+
+} 
+.nav>ul>li{
+    display: inline-block;
+    width: 120px;
+    text-align: center;
+    height: 50px;
+    position: relative;
+    line-height: 50px;
+    margin-top: 15px;
+    box-sizing: border-box;
+    /* border-radius: 5px; */
+ color: #e8f7fe; 
+}
+/* 選擇框 */
+.nav>ul>li:hover{
+    box-shadow: -10px 0px 15px #034c6a inset, 
+    0px -10px 15px #034c6a inset,
+    10px 0px 15px #034c6a inset, 
+    0px 10px 15px #034c6a inset;
+
+    box-sizing: border-box;
+}
+.nav>ul>li i{
+    width: 16px;
+    height: 16px;
+    display: inline-block;
+    position: relative;
+    top:3px;
+    margin-right: 5px;
+}
+.nav>ul>li>a{
+    color: #ffffff;
+    /* display: inline-block;
+    padding: 0 15px 0 5px; */
+    font-size: 14px;
+}
+
+.li_ul{
+    position: absolute;
+    background-color: #030829;
+    width: 100%;
+    border-top:4px solid #4b8df8;
+    display: none;
+    z-index: 999;
+
+}
+.li_ul li{
+    line-height: 40px;
+}
+.li_ul li:hover{
+    background-color: #4b8df8;
+}
+.li_ul li a{
+    color: #ffffff;
+    font-size: 13px;
+}
+
+.nav_1{
+     background-image: url("../WasteResuseInfo/img/nav_1.png");
+ }
+
+ .nav_2{
+     background-image: url("../WasteResuseInfo/img/nav_2.png");
+ }
+
+ .nav_3{
+     background-image: url("../WasteResuseInfo/img/nav_3.png");
+ }
+
+ .nav_4{
+     background-image: url("../WasteResuseInfo/img/nav_4.png");
+ }
+
+ .nav_5{
+     background-image: url("../WasteResuseInfo/img/nav_5.png");
+ }
+
+ .nav_6{
+     background-image: url("../WasteResuseInfo/img/nav_6.png");
+ }
+
+ .nav_7{
+     background-image: url("../WasteResuseInfo/img/nav_7.png");
+ }
+
+ .nav_8{
+     background-image: url("../WasteResuseInfo/img/nav_9.png");
+ }
+
+.nav_active{
+    border-bottom: 4px solid #4b8df8;
+    box-shadow: -10px 0px 15px #034c6a inset, 
+    0px -10px 15px #034c6a inset, 
+    10px 0px 15px #034c6a inset, 
+    0px 10px 15px #034c6a inset;
+
+    box-sizing: border-box;
+}
+</style>
